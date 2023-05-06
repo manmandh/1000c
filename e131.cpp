2db8a1cd2b65c77457ef2b5de8b5bee76e386341
Marry <117198229+manmandh@users.noreply.github.com>
2023-05-06 21:22:43 +0700

    updated

--- e131.cpp
+++ e131.cpp
@@ -386,6 +386,20 @@ void ex155(int a[], int n){
     cout << a[index] << endl;
 }
 
+void ex156(int a[], int n){
+    int space = INT_MAX;
+    int index = 0;
+    int x; cout << "enter x = "; cin >> x;
+    for(int i = 0; i < n; i++) {
+        int space2 = abs(a[i] - x);
+        if(space2 > space) {
+            space = space2;
+            index = i;
+        }
+    }
+    cout << a[index] << endl;
+}
+
 void ex157(int a[], int n){
     int min = INT_MAX;
     int max = INT_MIN;
@@ -491,22 +505,22 @@ void ex166(int a[], int n){
     }
 }
 
-bool isOddNumber(int n){
-    while(n>0){
-        int k = n%10;
-        if(k%2==0){
-            return false;
-        }
-        n/=10;
-    }
-    return true;
-}
+// bool isOddNumber(int n){
+//     while(n>0){
+//         int k = n%10;
+//         if(k%2==0){
+//             return false;
+//         }
+//         n/=10;
+//     }
+//     return true;
+// }
 
 int ex167(int a[], int n){
     bool check = false;
     int max = INT_MIN;
     for(int i=0; i<n;i++){
-        if(isOddNumber(a[i]) && a[i]>max){
+        if(isAllOddNumber(a[i]) && a[i]>max){
             max=a[i];
             check = true;
         }
@@ -574,6 +588,345 @@ void ex170(int a[], int n){
     }
     cout << prime << endl;
 }
+
+void ex171(int a[], int n){
+    
+}
+
+
+void ex173(int a[], int n){
+    int digit[10] = {0};
+    for(int i = 0; i < n; i++){
+        int tmp = a[i];
+        while(tmp){
+            digit[tmp%10]++;
+            tmp/=10;
+        }
+    }
+    int index = 0;
+    int min = digit[0];
+    for(int i=0;i<n;i++){
+        if(min > digit[i]){
+            min = digit[i];
+            index  = i;
+        }
+    }
+    cout << index << endl;
+} 
+
+void ex174(int a[], int n){
+    for(int i=0; i<n;i++){
+        for(int j=0; j<n; j++){
+            if(a[i]<=a[j]){
+                cout << a[i] << " " << a[j] << endl;
+            }
+        }
+    }
+}
+
+void ex175(int a[], int n){
+    int space = INT_MAX;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            int space2 = abs(a[i] - a[j]);
+            if(space2 < space){
+                space = space2;
+            }
+        }
+    }
+    cout << space << endl;
+}
+
+void ex176(int a[], int n){
+    for(int i=0; i<n; i++){
+        if(a[i]<0){
+            cout << a[i] << " ";
+        }
+    }
+}
+
+void ex177(int a[], int n, int x, int y){
+    cin >> x >> y;
+     for(int i = 0; i<n; i++){
+        if(a[i]>=x&&a[i]<=y){
+            cout << a[i] << endl;
+        }
+     }
+}
+
+void ex178(int a[], int n, int x, int y){
+    cin >> x >> y;
+     for(int i = 0; i<n; i++){
+        if(a[i]>=x&&a[i]<=y&&a[i]%2==0){
+            cout << a[i] << endl;
+        }
+     }
+}
+
+void ex179(int a[], int n){
+    for(int i=0; i<n-1; i++){
+        if(a[i]> abs(a[i+1])){
+            cout << a[i] << " ";
+        }
+    }
+}
+
+void ex180(int a[], int n){
+    for(int i=1; i<n-1; i++){
+        if(a[i]< abs(a[i+1]) && a[i] > a[i-1]){
+            cout << a[i] << " ";
+        }
+    }
+}
+
+void ex181(int a[], int n){
+    for(int i=1;i<n -1;i++){
+        if(!(a[i] % 2)) {
+            if(!(a[i + 1] % 2) || !(a[i - 1] % 2)) {
+                cout << a[i] << " ";
+            }
+        }
+    }
+}
+
+void ex182(int a[], int n){
+    for(int i=1;i<n -1;i++){
+        if(a[i]*a[i+1] < 0|| a[i]*a[i-1] <0){
+            cout << a[i] << endl;
+        }
+    }
+}
+
+void ex183(int a[], int n){
+    int max = a[0];
+    for(int i=1;i<n;i++){
+        if(max < a[i]){
+            max = a[i];
+        }
+    }
+    for(int i=0; i<n;i++){
+        if(max==a[i]){
+            cout << i << " ";
+        }
+    }
+}
+
+void ex184(int a[], int n){
+    for(int i=0; i<n; i++){
+        if(isCheckPrime(i)==a[i]){
+            cout << a[i] << " ";
+        }
+    }
+}
+ 
+void ex185(int a[], int n){
+    for(int i=0; i<n;i++){
+        if((int)sqrt(a[i])==sqrt(a[i])){
+            cout << a[i] << " ";
+        }
+    }
+}
+
+void ex186(int a[], int n){
+    for(int i=0; i<n;i++){
+        if(a[i]<0){
+            cout << i << endl;
+            break;
+        }
+    }
+}
+
+void ex187(int a[], int n){
+    int min = INT_MAX;
+    int pos = 0;
+    for(int i=0; i<n;i++){
+        if(a[i]>0){
+            if(a[i] < min){
+                min = a[i];
+                pos = i;
+            }
+        }
+    }
+    cout << pos << endl;
+}
+
+void ex188(int a[], int n){
+    int max = INT_MIN;
+    int pos = 0;
+    for(int i=0; i<n; i++){
+        if(a[i]%2==0){
+            if(a[i] > max){
+                max = a[i];
+                pos = i;
+            }
+        }
+    }
+    cout << pos << endl;
+}
+
+bool isOddNumber(int n){
+    while(n / 10){
+        n/=10;
+    }
+    return (n%2 != 0);
+}
+
+bool isCheckPrime(int n){
+    for(int i=2; i <= sqrt(n); i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return n>1;
+}
+
+void ex189(int a[], int n){
+    for(int i=0; i<n; i++){
+        if(isOddNumber(a[i]) && isCheckPrime(a[i])){
+            cout << a[i] << endl;
+        }
+    }
+}
+
+bool isAllOddNumber(int n){
+    while(n !=0){
+        int k = n % 10;
+        if(k%2==0){
+            return false;
+        }
+        n/=10;
+    }
+    return true;
+}
+
+void ex190(int a[],int n){
+    for(int i=0; i<n; i++){
+        if(isAllOddNumber(a[i])){
+            cout << a[i] << " ";
+        }
+    }
+}
+
+void ex191(int a[], int n){
+    //int max = INT_MIN;
+    for(int i=0; i<n;i++){
+        if(a[i]>a[i+1]&&(a[i]>a[i-1])){
+            cout << a[i] << " ";
+        }
+    }
+}
+
+bool isEvenNumber(int n){
+    while(n/10){
+        n/=10;
+    }
+    return (n%2 == 0);
+}
+
+void ex192(int a[], int n){
+    for(int i=0; i<n;i++){
+        if(isEvenNumber(a[i])){
+            cout << a[i] << " ";
+        }
+    }
+}
+
+
+int isPowerOfThree(int n) {
+    double k = log(n) / log(3);
+    return (int)k == k;
+}
+
+void ex193(int a[], int n) {
+    bool check = false;
+    for (int i = 0; i < n; i++) {
+        if (isPowerOfThree(a[i])) {
+            cout << a[i] << " ";
+            check = true;
+        }
+    }
+    if (!check) {
+        cout << 0;
+    }
+}
+
+
+void ex194(int a[], int n){
+    int space = INT_MAX;
+    for(int i=0; i<n;i++){
+        for(int j=0;j<n;j++){
+            int space2 = abs(a[i]-a[j]);
+            if(space2 < space){
+                space2 = space;
+            }
+        }
+    }
+    for(int i=0; i<n;i++){
+        for(int j=0;j<n;j++){
+            int space2 = abs(a[i]-a[j]);
+            if(space2 == space){
+                cout << a[i] << a[j] << " ";
+            }
+        }
+    }
+}
+
+void ex195(int a[], int n){
+    for(int i=0; i<n-2;i++){
+        for(int j=i+1; j<n-1; j++){
+            for(int k=0; k<n; k++){
+                if(a[i]==a[j]+a[k]){
+                    cout << a[i] << a[j] << a[k] << " ";
+                }
+            }
+        }
+    }
+}
+
+void ex196(int a[], int n){
+    for(int i=0;i<n;i++){
+        if(a[i]<0){
+            cout << a[i] << " ";
+        }
+    }
+}
+
+void ex197(int a[], int n){
+    for(int i=0; i<n;i++){
+        if(isOddNumber(a[i])){
+            cout << a[i] << " ";
+        }
+    }
+}
+
+void ex198(int a[], int n){
+    int max= a[0];
+    int pos = 0;
+    for(int i=0;i<n;i++){
+        if(max>a[i]){
+            max=a[i];
+            pos = i;
+        }
+    }
+    cout << pos << " ";
+}
+
+void ex199(int a[], int n){
+    for(int i=0;i<n;i++){
+        if(isCheckPrime(a[i])){
+            cout << a[i] << " ";
+        }
+    }
+}
+
+void ex200(int a[], int n){
+    double sum = 0;
+    for(int i=0;i<n;i++){
+        sum+=a[i];
+    }
+    cout << sum << " ";
+}
+
 
 
 
